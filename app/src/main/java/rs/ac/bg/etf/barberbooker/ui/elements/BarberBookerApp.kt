58083280dd52bd7e3fb4add92721704e9c4307db
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2024-07-18 13:20:35 +0200

    Login data stored in shared preferences.

--- app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/BarberBookerApp.kt
+++ app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/BarberBookerApp.kt
@@ -1,9 +1,22 @@
 package rs.ac.bg.etf.barberbooker.ui.elements
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -19,60 +32,88 @@ import rs.ac.bg.etf.barberbooker.ui.elements.screens.guest.registration.SignUpAs
 import rs.ac.bg.etf.barberbooker.ui.elements.screens.guest.registration.SignUpScreen
 import rs.ac.bg.etf.barberbooker.ui.elements.screens.user.barber.BarberInitialScreen
 import rs.ac.bg.etf.barberbooker.ui.elements.screens.user.client.ClientInitialScreen
+import rs.ac.bg.etf.barberbooker.ui.stateholders.BarberBookerViewModel
 
 @Composable
-fun BarberBookerApp() {
+fun BarberBookerApp(
+    barberBookerViewModel: BarberBookerViewModel = hiltViewModel()
+) {
     val navHostController = rememberNavController()
+    val context = LocalContext.current
+    val barberBookerActivity = context as Activity?
+    val uiState by barberBookerViewModel.uiState.collectAsState()
 
-    Scaffold {
-        paddingValues ->
-        NavHost(
-            navController = navHostController,
-            startDestination = staticRoutes[0],
-            modifier = Modifier.padding(paddingValues)
-        ) {
-            composable(route = staticRoutes[0]) {
-                InitialScreen(navHostController)
-            }
-            composable(route = staticRoutes[1]) {
-                LogInScreen(navHostController)
-            }
-            composable(route = staticRoutes[2]) {
-                SignUpScreen(navHostController)
-            }
-            composable(route = staticRoutes[3]) {
-                SignUpAsClientScreen(navHostController)
-            }
-            composable(route = staticRoutes[4]) {
-                SignUpAsBarberScreen(navHostController)
-            }
-            composable(route = staticRoutes[5]) {
-                LogInAsClientScreen(navHostController)
-            }
-            composable(route = staticRoutes[6]) {
-                LogInAsBarberScreen(navHostController)
-            }
-            composable(
-                route = "${staticRoutes[7]}/{clientEmail}",
-                arguments = listOf(
-                    navArgument("clientEmail") {
-                        type = NavType.StringType
+    LaunchedEffect(Unit) {
+        barberBookerViewModel.updateStartDestination(context)
+    }
+
+    if (uiState.isInitialScreenLoading) {
+        Column(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primary)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {}
+    } else {
+        Scaffold {
+                paddingValues ->
+            NavHost(
+                navController = navHostController,
+                startDestination = uiState.startDestination,
+                modifier = Modifier.padding(paddingValues)
+            ) {
+                composable(route = staticRoutes[0]) {
+                    InitialScreen(navHostController)
+                }
+                composable(route = staticRoutes[1]) {
+                    LogInScreen(navHostController)
+                }
+                composable(route = staticRoutes[2]) {
+                    SignUpScreen(navHostController)
+                }
+                composable(route = staticRoutes[3]) {
+                    SignUpAsClientScreen(navHostController)
+                }
+                composable(route = staticRoutes[4]) {
+                    SignUpAsBarberScreen(navHostController)
+                }
+                composable(route = staticRoutes[5]) {
+                    LogInAsClientScreen(navHostController)
+                }
+                composable(route = staticRoutes[6]) {
+                    LogInAsBarberScreen(navHostController)
+                }
+                composable(
+                    route = "${staticRoutes[7]}/{clientEmail}",
+                    arguments = listOf(
+                        navArgument("clientEmail") {
+                            type = NavType.StringType
+                        }
+                    )
+                ) {navBackStackEntry ->
+                    val clientEmail = navBackStackEntry.arguments?.getString("clientEmail") ?: ""
+                    BackHandler {
+                        barberBookerActivity?.finish()
                     }
-                )
-            ) {navBackStackEntry ->
-                val clientEmail = navBackStackEntry.arguments?.getString("clientEmail") ?: ""
-                ClientInitialScreen(clientEmail)
-            }
-            composable(
-                route = "${staticRoutes[8]}/{barberEmail}",
-                arguments = listOf(
-                    navArgument("barberEmail") {
-                        type = NavType.StringType
+                    barberBookerViewModel.updateLoginData(context, true, clientEmail, "client")
+                    ClientInitialScreen(clientEmail)
+                }
+                composable(
+                    route = "${staticRoutes[8]}/{barberEmail}",
+                    arguments = listOf(
+                        navArgument("barberEmail") {
+                            type = NavType.StringType
+                        }
+                    )
+                ) {navBackStackEntry ->
+                    val barberEmail = navBackStackEntry.arguments?.getString("barberEmail") ?: ""
+                    BackHandler {
+                        barberBookerActivity?.finish()
                     }
-                )
-            ) {navBackStackEntry ->
-                val barberEmail = navBackStackEntry.arguments?.getString("barberEmail") ?: ""
-                BarberInitialScreen(barberEmail)
+                    barberBookerViewModel.updateLoginData(context, true, barberEmail, "barber")
+                    BarberInitialScreen(barberEmail)
+                }
             }
         }
     }
---
+++ app/src/main/java/rs/ac/bg/etf/barberbooker/ui/stateholders/BarberBookerViewModel.kt
@@ -0,0 +1,58 @@
+package rs.ac.bg.etf.barberbooker.ui.stateholders
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import rs.ac.bg.etf.barberbooker.data.staticRoutes
+import javax.inject.Inject
+
+data class BarberBookerUiState(
+    var startDestination: String = staticRoutes[0],
+    var isInitialScreenLoading: Boolean = true
+)
+
+@HiltViewModel
+class BarberBookerViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(BarberBookerUiState())
+    val uiState = _uiState
+
+    fun updateLoginData(
+        context: Context,
+        isLoggedIn: Boolean,
+        userEmail: String = "",
+        userType: String = ""
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("login_data", Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putBoolean("is_logged_in", isLoggedIn)
+            putString("user_email", userEmail)
+            putString("user_type", userType)
+            apply()
+        }
+    }
+
+    fun updateStartDestination(context: Context) = viewModelScope.launch(Dispatchers.IO) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("login_data", Context.MODE_PRIVATE)
+        val isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false)
+        val userEmail = sharedPreferences.getString("user_email", "")
+        val userType = sharedPreferences.getString("user_type", "")
+
+        val startDestination = when {
+            isLoggedIn && userType == "client" -> "${staticRoutes[7]}/$userEmail"
+            isLoggedIn && userType == "barber" -> "${staticRoutes[8]}/$userEmail"
+            else -> staticRoutes[0]
+        }
+
+        withContext(Dispatchers.Main) {
+            _uiState.update { it.copy(startDestination = startDestination, isInitialScreenLoading = false) }
+        }
+    }
+}
