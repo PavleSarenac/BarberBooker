56963e623c11f5ba4706a45be37931599933a3c3
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2024-07-29 19:21:22 +0200

    Improved padding when showing reviews.

--- app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/screens/user/barber/BarberReviewsScreen.kt
+++ app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/screens/user/barber/BarberReviewsScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -132,6 +134,7 @@ fun BarberReviewsScreen(
                         }
                     }
                 }
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
     }
