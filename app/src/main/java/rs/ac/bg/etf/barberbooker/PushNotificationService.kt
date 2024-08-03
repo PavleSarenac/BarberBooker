30ae03cbd68a42bbd0d21aea4379bba90f49a86e
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2024-08-03 16:19:07 +0200

    Fcm token updated in the database when the token changes.

--- app/src/main/java/rs/ac/bg/etf/barberbooker/PushNotificationService.kt
+++ app/src/main/java/rs/ac/bg/etf/barberbooker/PushNotificationService.kt
@@ -1,25 +1,67 @@
 package rs.ac.bg.etf.barberbooker
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.messaging
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import rs.ac.bg.etf.barberbooker.data.retrofit.entities.structures.FcmTokenUpdateData
+import rs.ac.bg.etf.barberbooker.data.retrofit.repositories.BarberRepository
+import rs.ac.bg.etf.barberbooker.data.retrofit.repositories.ClientRepository
+import javax.inject.Inject
 
 class PushNotificationService: FirebaseMessagingService() {
 
+    @Inject
+    lateinit var barberRepository: BarberRepository
+
+    @Inject
+    lateinit var clientRepository: ClientRepository
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.IO + serviceJob)
+
+    /*
+    * "token" needs to be sent to the server so that the token in the database for the
+    * currently logged in user can be updated so that push notifications can be sent from
+    * the server to that specific user even though the token has changed
+    * */
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        /*
-        * TODO
-        * "token" needs to be sent to the server so that the token in the database for the
-        * currently logged in user can be updated so that push notifications can be sent from
-        * the server to that specific user even though the token has changed
-        * */
+        serviceScope.launch(Dispatchers.IO) {
+            val sharedPreferences: SharedPreferences = getSharedPreferences("login_data", Context.MODE_PRIVATE)
+            val isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false)
+            val userEmail = sharedPreferences.getString("user_email", "")
+            val userType = sharedPreferences.getString("user_type", "")
+
+            if (isLoggedIn) {
+                if (userType == "barber") {
+                    barberRepository.updateFcmToken(
+                        FcmTokenUpdateData(
+                            email = userEmail!!,
+                            fcmToken = Firebase.messaging.token.await()
+                        )
+                    )
+                } else {
+                    clientRepository.updateFcmToken(
+                        FcmTokenUpdateData(
+                            email = userEmail!!,
+                            fcmToken = Firebase.messaging.token.await()
+                        )
+                    )
+                }
+            }
+        }
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-
-        // This method can be used for defining a response to receiving the notification from Firebase.
-        // Probably won't be necessary for this app.
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
     }
 
 }
