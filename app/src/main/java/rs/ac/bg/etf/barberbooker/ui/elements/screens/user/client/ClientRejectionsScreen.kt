3a62527b5560290d080a1cc8663261c95bc387a0
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2025-10-03 14:44:47 +0200

    feat: [ClientRejectionsScreen] requests' data enriched

--- app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/screens/user/client/ClientRejectionsScreen.kt
+++ app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/screens/user/client/ClientRejectionsScreen.kt
@@ -2,12 +2,22 @@ package rs.ac.bg.etf.barberbooker.ui.elements.screens.user.client
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.ContentCut
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -18,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -61,16 +72,55 @@ fun ClientRejectionsScreen(
                 val currentRequest = clientRejectionsUiState.rejections[it]
                 ListItem(
                     headlineContent = {
-                        Text(
-                            text = currentRequest.barbershopName,
-                            color = MaterialTheme.colorScheme.onPrimary
-                        )
+                        Row {
+                            Icon(
+                                imageVector = Icons.Filled.ContentCut,
+                                contentDescription = "Barbershop name",
+                                modifier = Modifier
+                                    .size(10.dp)
+                                    .align(Alignment.CenterVertically),
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(
+                                text = currentRequest.barbershopName,
+                                color = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
                     },
                     supportingContent = {
-                        Text(
-                            text = "${currentRequest.date}, ${currentRequest.startTime} - ${currentRequest.endTime}",
-                            color = MaterialTheme.colorScheme.onPrimary
-                        )
+                        Column {
+                            Row {
+                                Icon(
+                                    imageVector = Icons.Filled.LocationOn,
+                                    contentDescription = "Location",
+                                    modifier = Modifier
+                                        .size(10.dp)
+                                        .align(Alignment.CenterVertically),
+                                    tint = MaterialTheme.colorScheme.onPrimary
+                                )
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(
+                                    text = "${currentRequest.barberMunicipality}, ${currentRequest.barberCity}",
+                                    color = MaterialTheme.colorScheme.onPrimary
+                                )
+                            }
+                            Row {
+                                Icon(
+                                    imageVector = Icons.Filled.AccessTime,
+                                    contentDescription = "Date & Time",
+                                    modifier = Modifier
+                                        .size(10.dp)
+                                        .align(Alignment.CenterVertically),
+                                    tint = MaterialTheme.colorScheme.onPrimary
+                                )
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(
+                                    text = "${currentRequest.date}, ${currentRequest.startTime} - ${currentRequest.endTime}",
+                                    color = MaterialTheme.colorScheme.onPrimary
+                                )
+                            }
+                        }
                     },
                     modifier = Modifier
                         .fillMaxWidth()
