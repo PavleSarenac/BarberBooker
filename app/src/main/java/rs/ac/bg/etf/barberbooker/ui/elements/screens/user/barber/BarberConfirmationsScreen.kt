69d1018f2b270a6c0d7515b544f2bab36e598879
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2025-10-07 16:42:29 +0200

    feat: [BarberConfirmationsScreen] UI improvements

--- app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/screens/user/barber/BarberConfirmationsScreen.kt
+++ app/src/main/java/rs/ac/bg/etf/barberbooker/ui/elements/screens/user/barber/BarberConfirmationsScreen.kt
@@ -3,11 +3,16 @@ package rs.ac.bg.etf.barberbooker.ui.elements.screens.user.barber
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Divider
@@ -24,6 +29,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -66,16 +72,38 @@ fun BarberConfirmationsScreen(
                 val currentRequest = barberConfirmationsUiState.confirmations[it]
                 ListItem(
                     headlineContent = {
-                        Text(
-                            text = "${currentRequest.clientName} ${currentRequest.clientSurname}",
-                            color = MaterialTheme.colorScheme.onPrimary
-                        )
+                        Row {
+                            Icon(
+                                imageVector = Icons.Filled.AccountCircle,
+                                contentDescription = "Client Name",
+                                modifier = Modifier
+                                    .size(10.dp)
+                                    .align(Alignment.CenterVertically),
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(
+                                text = "${currentRequest.clientName} ${currentRequest.clientSurname}",
+                                color = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
                     },
                     supportingContent = {
-                        Text(
-                            text = "${currentRequest.date}, ${currentRequest.startTime} - ${currentRequest.endTime}",
-                            color = MaterialTheme.colorScheme.onPrimary
-                        )
+                        Row {
+                            Icon(
+                                imageVector = Icons.Filled.AccessTime,
+                                contentDescription = "Date & Time",
+                                modifier = Modifier
+                                    .size(10.dp)
+                                    .align(Alignment.CenterVertically),
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(
+                                text = "${currentRequest.date}, ${currentRequest.startTime} - ${currentRequest.endTime}",
+                                color = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
                     },
                     trailingContent = {
                         Row {
